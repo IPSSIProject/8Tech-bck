b09976fbbcb0dd22d25e4a6ca911e903e09d1d8d
Thomas <thomas.jallu@flowbird.group>
2022-08-08 17:42:10 +0200

    error on Port when deploy

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const createServer = require('./server');
 
 const app = createServer();
-const PORT = 4000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
